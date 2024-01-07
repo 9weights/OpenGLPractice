51a6d3219065a5e178cfefaac1cab9800d1b3bb9
9weights <68071656+9weights@users.noreply.github.com>
2024-01-07 00:20:05 +0000

    textures working

--- textureTry.cpp
+++ textureTry.cpp
@@ -1,8 +1,11 @@
+//# Includes
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Shader.h"
 #include <iostream>
 #include <filesystem>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image/stb_image.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -13,6 +16,30 @@ void processInput(GLFWwindow *window)
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+unsigned int textureSetup(const char* path)
+{
+    int width, height, nrChannels;
+    unsigned char* textureData = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (!textureData)
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(textureData);
+    return textureID;
+}
+
 int main()
 {
     //# Window
@@ -35,54 +62,73 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+
+    //# Texture stuff (containter)
+    unsigned int textureID{textureSetup("/home/vimto/chalf/OpenGLPractice/texture_source/container.jpg")};
+
     //# Create vertex and index info
-    float vertices[] = { // outside render loop as static?
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-    }; 
-    unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 2   // first triangle
+    // Square
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
-    //# Create glsl objects
-    unsigned int VBO;
-    unsigned int EBO;
-    unsigned int VAO;
-    // try mess around with these - e.g. gen all buffers and then bind all buffers, gen and bind 1 at time,
-    // gen vertex array after others, bind vertex array after others
+    unsigned int indices[] = {
+        0,1,3,
+        1,2,3
+    };
+    //# Config buffer and array
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
+
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
+    
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
-    glEnableVertexAttribArray(0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    //std::string currentDirectory{std::filesystem::current_path()};
-    //std::string tempString1 = currentDirectory + "/texture.vs";
-    //std::string tempString2 = currentDirectory + "/texture.fs";
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+
+    //# Shader stuff
     const char* vertexPath{"/home/vimto/chalf/OpenGLPractice/texture.vs"};
     const char* fragmentPath{"/home/vimto/chalf/OpenGLPractice/texture.fs"};
     Shader shaderProgram(vertexPath, fragmentPath);
+
     //# Render Loop
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
-        glUseProgram(shaderProgram.programID);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+
+        glUseProgram(shaderProgram.programID);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
